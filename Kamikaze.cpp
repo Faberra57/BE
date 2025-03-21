5d30d8cdd15e215416218740b85e6dedd1b9d955
nacer605 <130840153+nacer605@users.noreply.github.com>
2025-03-21 16:11:54 +0100

    new changes

--- Kamikaze.cpp
+++ Kamikaze.cpp
@@ -3,13 +3,13 @@
 #include <cmath>
 
 void Kamikaze::Deplacer(Bestiole& B1,Milieu& milieu,std::vector<Bool> detection){
-    std::vector<Bool> detection = B1.Detecter(); //définir la méthode detecter qui renvoi la détéction via des bool de toutes les bestioles de l'aquarium
+    std::vector<Bool> best_detectés = B1.detection(milieu); //définir la méthode detecter qui renvoi la détéction via des bool de toutes les bestioles de l'aquarium
     double dist_min = std::numeric_limits<double>::infinity();
     int position_bestiole_proche = 0;
     for (std::size_t i = 0; i< detection.size();i++){
         if (detection[i]){
             Bestiole B2 = milieu.vecteur_bestioles[i]; //besoin d'une méthode qui récupère la i-ème bestiole de l'aquarium
-            double tmp = (B1.get_x() - B2.get_x())^2 + (B1.get_y() - B2.get_y())^2 ; 
+            double tmp = pow((B1.get_x() - B2.get_x()),2) + pow((B1.get_y() - B2.get_y()),2) ; 
             if (tmp<dist_min){
                 dist_min = tmp;
                 position_bestiole_proche = i;
@@ -20,7 +20,9 @@ void Kamikaze::Deplacer(Bestiole& B1,Milieu& milieu,std::vector<Bool> detection)
     double dx = B1.get_x() - milieu.vecteur_bestioles[position_bestiole_proche].get_x();
     double dy = B1.get_y() - milieu.vecteur_bestioles[position_bestiole_proche].get_y();
 
-    B1.setOrientation(std::atan2(dy,dx) * 180 / M_PI);
-    B1.setVitesse(B1.getVitesse();
+    if (dx != 0 || dy!= 0 ){
+        B1.setOrientation(std::atan2(dy,dx) * 180 / M_PI);
+        B1.setVitesse(B1.getVitesse());
+    }
 }
 
