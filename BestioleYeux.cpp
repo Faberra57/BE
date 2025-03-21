f3ac1cac1a42731a7035af6ca592e5f3c6b951de
Faberra57 <thomas.favre57@gmail.Com>
2025-03-21 16:08:10 +0100

    fix BestioleYeux

--- BestioleYeux.cpp
+++ BestioleYeux.cpp
@@ -18,39 +18,13 @@ BestioleYeux::BestioleYeux(const double delta_min,const double delta_max,
     gamma = (gamma_max - gamma_min) * ((double)rand() / RAND_MAX) + gamma_min;
 }
 
-double angleEntreVecteurs(double Ax, double Ay, double Bx, double By) {
-
-    // Produit scalaire
-    double produitScalaire = Ax * Bx + Ay * By;
-
-    // Normes des vecteurs
-    double normeA = std::sqrt(Ax * Ax + Ay * Ay);
-    double normeB = std::sqrt(Bx * Bx + By * By);
-
-    // Vérification pour éviter une division par zéro
-    if (normeA == 0 || normeB == 0) {
-        return 0;  // Aucun angle défini si l'un des vecteurs est nul
-    }
-
-    // Calcul de l'angle en radians
-    double cosTheta = produitScalaire / (normeA * normeB);
-    
-    // Sécurité pour éviter des erreurs d'arrondi
-    if (cosTheta > 1) cosTheta = 1;
-    if (cosTheta < -1) cosTheta = -1;
-
-    return std::abs(std::acos(cosTheta));  // Valeur absolue de l'angle en radians
-}
-
-
-
 std::vector<bool> BestioleYeux::detecter(const Milieu& milieu)
 {
     std::vector<bool> res;
     for (int i = 0; i < milieu.getNbBestioles(); i++)
     {
         double distance = sqrt(pow(owner.getX() - milieu.getBestiole(i).getX(), 2) + pow(owner.getY()  - milieu.getBestiole(i).getY(), 2));
-        double angle = angleEntreVecteurs(owner.getVx(), owner.getVy(), milieu.getBestiole(i).getX() - owner.getX(), milieu.getBestiole(i).getY() - owner.getY());
+        double angle = std::abs(owner.getOrientation() - milieu.getBestiole(i).getOrientation());
         if (distance <= delta && angle <= alpha/2 && (double)rand() / RAND_MAX <= gamma) //alpha/2 car on prend la valeur absolue de l'angle entre les deux vecteurs vitesse
         {
             res.push_back(true);
