2875db9c3c6a176974005a828050d6ca3c45f7dc
nacer605 <130840153+nacer605@users.noreply.github.com>
2025-03-21 16:10:33 +0100

    Repush Peureuse ; changement Fx,Fy

--- Peureuse.cpp
+++ Peureuse.cpp
@@ -6,22 +6,26 @@ Peureuse::Peureuse(){}
 
 
 void Peureuse::Deplacer(Bestiole& B1,Milieu& milieu){
-    std::vector<Bool> detection = B1.Detecter();
-    double seuil = 10; // seuil à définir en général
+    std::vector<Bool> best_detectés = B1.detection(milieu);
+    double seuil = 10; // seuil à définir dans le JSON, sert à établir le nombre maximal de bestioles environnates détécté au bout duquel la la bestiole Peureuse fuit
     int nb_bestiole_proches = 0;
     double Fx = 0;
     double Fy = 0;
-    for (std::size_t i = 0; i< detection.size();i++){
-        if (detection[i]){
+    for (std::size_t i = 0; i< best_detectés.size();i++){
+        if (best_detectés[i]){
             nb_bestiole_proches++;
-            Fx = Fx + (B1.get_x() - detection[i].get_x());
-            Fy = Fy + (B1.get_y() - detection[i].get_y());
+            Fx = Fx + (B1.get_x() - best_detectés[i].get_x());
+            Fy = Fy + (B1.get_y() - best_detectés[i].get_y());
         }
     }
+    Fx = Fx / nb_bestiole_proches;
+    Fy = Fy / nb_bestiole_proches;
     if (nb_bestiole_proches>seuil){
-        B1.setOrientation(std::atan2(Fy,Fx) * 180 / M_PI + 180);
-        B1.setVitesse(B1.getVitesse() * 2); //multiplicateur à définir en amont
+        if (Fx != 0 || Fy!= 0 ){
+            B1.setOrientation(std::atan2(Fy,Fx) * 180 / M_PI + 180);
+            B1.setVitesse(B1.getVitesse() * multiplicateur_vit_peureuse); //multiplicateur à définir dans le JSON
     }
+}
 }
 
 //vecteur detection obtenu via une méthode qu'on implemente sur la classe bestiole
