6378e69303e50057dc7ef4c6fdd343838480b5c6
nacer605 <130840153+nacer605@users.noreply.github.com>
2025-03-21 14:45:45 +0100

    Create Kamikaze.h

---
+++ Kamikaze.h
@@ -0,0 +1,12 @@
+#ifndef KAMIKAZE
+#define KAMIKAZE
+
+#include "IComportement.h"
+
+
+class Kamikaze{
+    public :
+        void Deplacer(Bestiole Bestiole,Milieu milieu,std::vector<Bool> detection);
+};
+
+#endif
