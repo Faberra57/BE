5d9df5b09c3d636bb3015a934e0aa05dd806173b
nacer605 <130840153+nacer605@users.noreply.github.com>
2025-03-21 14:45:22 +0100

    Create Peureuse.h

---
+++ Peureuse.h
@@ -0,0 +1,12 @@
+#ifndef PEUREUSE
+#define PEUREUSE
+
+#include "IComportement.h"
+
+
+class Peureuse{
+    public :
+        void Deplacer(Bestiole& Bestiole,Milieu milieu,std::vector<Bool> detection);
+};
+
+#endif
