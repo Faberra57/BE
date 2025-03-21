7e79d457eae627fdbbcbf21d35992df043e18fe9
nacer605 <130840153+nacer605@users.noreply.github.com>
2025-03-21 14:44:55 +0100

    Create Peureuse.cpp

---
+++ Peureuse.cpp
@@ -0,0 +1,27 @@
+#include "Peureuse.h"
+#include <iostream>
+#include <cmath>
+
+Peureuse::Peureuse(){}
+
+
+void Peureuse::Deplacer(Bestiole& B1,Milieu milieu,std::vector<Bool> detection){
+    double seuil = 10;
+    int nb_bestiole_proches = 0;
+    double Fx = 0;
+    double Fy = 0;
+    for (std::size_t i = 0; i< dectection.size();i++){
+        if (detection[i]){
+            nb_bestiole_proches++;
+            Fx = Fx + (B1.position_x - detection[i].position_x);
+            Fy = Fy + (B1.position_y - detection[i].position_y);
+        }
+    }
+    if (nb_bestioles_proches>seuil){
+        B1.orientation = std::atan2(Fy,Fx) * 180 / M_PI + 180;
+        B1.vitesse = B1.vitesse * 2;
+    }
+}
+
+//vecteur detection obtenu via une méthode qu'on implemente sur la classe bestiole
+//tout mettre en privé pour les attributs, rajouter des setteurs/getteurs
